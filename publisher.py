19f0090fc1e2b09b1e56e24c6dd635cdea2a3a48
Dan Fornika <dfornika@gmail.com>
2020-10-02 13:59:40 -0700

    Use RegexMatchingEventHandler to match miseq run dirs

--- publisher.py
+++ publisher.py
@@ -13,31 +13,33 @@ from sample_sheet import SampleSheet
 from pprint import pprint
 
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import RegexMatchingEventHandler
 
 from datetime import datetime
 
-class RunEventHandler(FileSystemEventHandler):
-    def __init__(self, socket):
-        self.recursive=False
+class RunEventHandler(RegexMatchingEventHandler):
+    def __init__(self, socket, regexes):
+        super().__init__(regexes)
         self.socket = socket
-        
+
     def on_created(self, event):
         topic = "illumina_runs"
+        experiment_name = ""
         now = datetime.now().isoformat()
         time.sleep(5)
+
         try:
             sample_sheet = SampleSheet(os.path.join(event.src_path, 'SampleSheet.csv'))
-        except FileNotFoundError as e:
+            sample_sheet_dict = json.loads(sample_sheet.to_json())
+            experiment_name = sample_sheet_dict['Header']['Experiment Name']
+        except Exception as e:
             print(e)
-
-        sample_sheet_dict = json.loads(sample_sheet.to_json())
 
         messagedata = {
             "timestamp": now,
             "event": "run_directory_created",
             "path": os.path.abspath(event.src_path),
-            "experiment_name": sample_sheet_dict['Header']['Experiment Name']
+            "experiment_name": experiment_name
         }
         message = json.dumps(messagedata)
         print("%s %s" % (topic, message))
@@ -60,7 +62,12 @@ def main(args):
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://*:%s" % args.port)
 
-    run_event_handler = RunEventHandler(socket)
+    miseq_run_dir_regex = ".+/\d{6}_[A-Z0-9]{6}_\d{4}_\d{9}-[A-Z0-9]{5}$"
+    illumina_run_dir_regexes = [
+        miseq_run_dir_regex,
+    ]
+
+    run_event_handler = RunEventHandler(socket, regexes=illumina_run_dir_regexes)
     observer = Observer()
     observer.schedule(run_event_handler, args.path, recursive=False)
     observer.start()
@@ -71,7 +78,7 @@ def main(args):
             heartbeat(socket)
     except KeyboardInterrupt:
         observer.stop()
-    observer.join()
+        observer.join()
 
 
 if __name__ == '__main__':

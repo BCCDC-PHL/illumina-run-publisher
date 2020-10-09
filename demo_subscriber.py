d8369d8c4a7148e50835fe705db7dda7db395039
Dan Fornika <dfornika@gmail.com>
2020-10-09 13:25:13 -0700

    Make keypairs optional (#14)
    * Make keypairs optional
    
    * Update usage to show keypairs are optional

--- demo_subscriber.py
+++ demo_subscriber.py
@@ -11,13 +11,15 @@ def main(args):
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
 
-    client_secret_file = args.private_key
-    client_public, client_secret = zmq.auth.load_certificate(client_secret_file)
-    socket.curve_secretkey = client_secret
-    socket.curve_publickey = client_public
-    server_public_file = args.public_key
-    server_public, _ = zmq.auth.load_certificate(server_public_file)
-    socket.curve_serverkey = server_public
+    if args.public_key and args.private_key:
+        client_secret_file = args.private_key
+        client_public, client_secret = zmq.auth.load_certificate(client_secret_file)
+        socket.curve_secretkey = client_secret
+        socket.curve_publickey = client_public
+        server_public_file = args.public_key
+        server_public, _ = zmq.auth.load_certificate(server_public_file)
+        socket.curve_serverkey = server_public
+
     print("Collecting updates from server...")
     socket.connect("tcp://127.0.0.1:%s" % args.port)
 
@@ -31,7 +33,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', default=5556)
-    parser.add_argument('--public_key', required=True)
-    parser.add_argument('--private_key', required=True)
+    parser.add_argument('--public_key')
+    parser.add_argument('--private_key')
     args = parser.parse_args()
     main(args)
